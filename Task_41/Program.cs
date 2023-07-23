e2575c2dcf38c669df21ed5139a0efdb32655da6
Sergey Zavyalov <ser500vms@gmail.com>
2023-07-23 15:38:09 +0300

    Выполнил задание №41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int CountMoreZiro(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum += 1;
+        }
+    }
+    return sum;
+}
+
+int[] GenerateUserArray(int size)
+{
+    int[] array = new int[size];
+    int num = 1;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {num}-е число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        num++;
+    }
+    return array;
+}
+
+int GetInput (string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = GetInput("Сколько чисел Вы хотите ввести? : ");
+int[] array = GenerateUserArray(size);
+int sum = CountMoreZiro(array);
+Console.Write($"{String.Join(", ", array)} -> {sum}");
+
